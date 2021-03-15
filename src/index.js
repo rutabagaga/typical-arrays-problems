3c96892b377b94d774cbab10a8cc76e57735409e
rutabagaga <komleva.darya@gmail.com>
2021-03-15 21:17:55 +0300

    feat: modified js file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 
 exports.min = function min (array) {
-	if (array == undefined || array.length === 0) {
-		return 0;
-  } else {
-		return Math.min(...array);
-  }
+  if (!array || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-	if (array == undefined || array.length === 0)  {
-		return 0;
-  } else {
-		return Math.max(...array);
+  if (!array || array.length === 0) return 0;
+  return Math.max(...array);;
 }
 
 exports.avg = function avg (array) {
-	if (array == undefined || array.length === 0)  {
-		 return 0;
-	} else {
-		 let arraySum = array.reduce((sum, current) => sum + current, 0)
-		 return arraySum/array.length;
-	}
+  if (!array || array.length === 0) return 0;
+  return (array.reduce((al, re) => al + re, 0) / array.length);
 }
